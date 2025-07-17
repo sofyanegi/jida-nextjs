f81940a6e3f321f901de4c62ab55869e6d4c83d7
Sofyan Egi <sofyanegil@gmail.com>
2025-07-17 12:54:40 +0700

    fix(eslint) : exclude prisma client for linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,11 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript', 'prisma')];
+const eslintConfig = [
+  {
+    ignores: ['src/generated/prisma/**/*'],
+  },
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+];
 
 export default eslintConfig;
